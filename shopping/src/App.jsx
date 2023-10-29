dd508409d0adba66ecb06b39874df00df92a2b21
kiminn <134191815+kiminn@users.noreply.github.com>
2023-10-29 17:43:07 +0900

    feat: route설정

--- shopping/src/App.jsx
+++ shopping/src/App.jsx
@@ -1,11 +1,17 @@
-
-
 function App() {
-  return (
-    <>        
-    
-    </>   
-  )       
-}         
+    return (
+        <>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route element={<DefaultLayout />}>
+                        <Route path="/" element={<Main />} />
+                        <Route path="/cart" element={<Cart />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
 
-export default App
+export default App;
